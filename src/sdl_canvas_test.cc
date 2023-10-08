4da6cc48b6afb5f3a6fb56dce784d4c4ce3c83b2
TheZacher5645 <zander5645@yahoo.com>
2023-10-08 09:38:17 -0500

    Working pressure input
    figured out the right way to do an emscripten main loop while passing in userData,

--- src/sdl_canvas_test.cc
+++ src/sdl_canvas_test.cc
@@ -1,50 +1,105 @@
+/*
+	TODO: Makefile
+	em++ sdl_canvas_test.cc -sUSE_SDL=2 -sEXPORTED_FUNCTIONS=_main,_setWinPressure -sEXPORTED_RUNTIME_METHODS=cwrap -o test.html
+*/
+
 #include <SDL2/SDL.h>
 #include <emscripten.h>
+#include <emscripten/html5.h>
+
+#include <iostream>
 #include <cmath>
 
 unsigned W = 800, H = 600;
 SDL_Window* window;
 SDL_Surface* surface;
-int cursorX = 0, cursorY = 0;
 bool quit = false;
 
-void draw() {
-	unsigned* pixels = (unsigned*)surface->pixels;
-	for (std::size_t i=0; i < W*H; i++) {
-		int x = i % W;
-		int y = i / W;
-		if (std::hypot(cursorX - x, cursorY - y) <= 10)
-			pixels[i] = SDL_MapRGB(surface->format, 255, 0, 128);
-		else
-			pixels[i] = SDL_MapRGB(surface->format, 200, 200, 200);
+struct AppState {
+	bool penPressed = false;
+	int penX = 0;
+	int penY = 0;
+	float penPressure = 1.0;
+	unsigned penSize = 10;
+};
 
+// SDL2 can't detect pen pressure it seems, we
+// use some Javascript to do that work for us.
+void startPressureListener() {
+	EM_ASM(
+		const setWinPressure = Module.cwrap("setWinPressure", "", ["number"]);
+		Module.canvas.addEventListener("pointerdown", event => setWinPressure(event.pressure));
+		Module.canvas.addEventListener("pointermove", event => setWinPressure(event.pressure));
+		Module.canvas.addEventListener("pointerup", event => setWinPressure(event.pressure));
+	);
+}
+
+float winPressure = 1.0;
+extern "C" {
+	void setWinPressure(float pressure) {
+		winPressure = pressure;
+	}
+}
+
+
+void draw(const AppState& s) {
+	unsigned* pixels = (unsigned*)surface->pixels;
+
+	for (unsigned y=0, i=0; y < H; y++     )
+	for (unsigned x=0     ; x < W; x++, i++) {
+		unsigned char r=200, g=200, b=200;
+
+		int dx = s.penX - x;
+		int dy = s.penY - y;
+		int sz = s.penSize * (s.penPressed ? s.penPressure : 1.0);
+		if (dx*dx + dy*dy <= sz*sz) {
+			if (!s.penPressed) r=255, g=  0, b=128;
+			else               r=  0, g=  0, b=  0;
+		}
+		pixels[i] = SDL_MapRGB(surface->format, r, g, b);
 	}
 
 	SDL_UpdateWindowSurface(window);
 }
 
-bool detectEvents() {
-	bool input = true;
-	for (SDL_Event ev; SDL_PollEvent(&ev); )
+bool detectEvents(AppState& s) {
+	bool input = false;
+	for (SDL_Event ev; SDL_PollEvent(&ev); input=true)
 	switch (ev.type) {
 		case SDL_QUIT:
-			quit = true; input = false;
+			quit = true;
 			break;
 		case SDL_MOUSEMOTION:
-			cursorX = ev.motion.x;
-			cursorY = ev.motion.y;
+			s.penX = ev.motion.x;
+			s.penY = ev.motion.y;
+			s.penPressure = winPressure;
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+			s.penPressed = true;
+			s.penPressure = winPressure;
+			break;
+		case SDL_MOUSEBUTTONUP:
+			s.penPressed = false;
+			s.penPressure = 0.0;
+			break;
 		case SDL_KEYDOWN:
 			switch (ev.key.keysym.sym) {
 				case SDLK_ESCAPE:
-					quit = true; input = false;
+					quit = true;
+					break;
+				case SDLK_LEFTBRACKET:
+					s.penSize = std::max(0u, s.penSize-1);
+					break;
+				case SDLK_RIGHTBRACKET:
+					s.penSize = std::min(20u, s.penSize+1);
 					break;
 			} break;
 	}
 	return input;
 }
 
-void gameLoop() {
-	if (detectEvents()) draw();
+void appLoopBody(AppState& s) {
+	if (detectEvents(s)) draw(s);
 }
 
 int main(int argc, char* argv[]) {
@@ -52,16 +107,23 @@ int main(int argc, char* argv[]) {
 
 	window = SDL_CreateWindow("Sketch Client",
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		W, H,
-		0);
+		W, H, SDL_WINDOW_SHOWN);
 	surface = SDL_GetWindowSurface(window);
 
-	draw();
+	AppState state {};
 
 #ifdef __EMSCRIPTEN__
-	emscripten_set_main_loop(gameLoop, 0, true);
+	startPressureListener();
+
+	emscripten_set_main_loop_arg(
+		[](void* s) {
+			appLoopBody(*(AppState*)s);
+		},
+		&state,
+		0, true
+	);
 #else
-	while (!quit) gameLoop();
+	while (!quit) appLoopBody(state);
 #endif
 
 	SDL_DestroyWindow(window);
